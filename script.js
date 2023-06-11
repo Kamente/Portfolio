80083b81be326f4409473bae5e463037eb8ec50a
Kamente <justinm48000@gmail.com>
2023-06-11 15:59:05 +0300

    Added timeline and javascript for the h1

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 const submit = document.getElementById("submit");
-submit.addEventListener("click", function(){
+submit.addEventListener("click", function () {
     alert("Thanks for submitting");
 });
+
+let nam = document.querySelector('span#name');
+let setName = "Justin";
+let i = 0;
+nam.innerHTML = "";
+
+let direction = "f"
+
+setInterval(() => {
+
+    if (direction == "f") {
+        if (i >= setName.length) {
+            i = setName.length - 1
+            direction = "b"
+            return
+        }
+        nam.innerHTML += setName[i];
+        i++;
+    } else {
+        if (i < 0) {
+            i = 0;
+            direction = "f";
+            return
+        }
+        nam.innerHTML = setName.substring(0, i);
+        i--;
+    }
+}, 200);
